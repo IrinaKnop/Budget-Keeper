e64775a356abdee73a362f744e9949c3f55ba130
IrinaKnop <knop.irina.d@gmail.com>
2021-10-08 21:14:51 +0300

    Short payments Stats

---
+++ src/main/java/org/knop/budgetKeeper/dto/PaymentsShortStatsDto.java
@@ -0,0 +1,14 @@
+package org.knop.budgetKeeper.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+public class PaymentsShortStatsDto {
+    private String category;
+    private BigDecimal value;
+    private Double percentage;
+}
--- src/main/java/org/knop/budgetKeeper/repository/PaymentRepository.java
+++ src/main/java/org/knop/budgetKeeper/repository/PaymentRepository.java
@@ -1,12 +1,15 @@
 package org.knop.budgetKeeper.repository;
 
+import org.knop.budgetKeeper.dto.PaymentDto;
 import org.knop.budgetKeeper.models.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Date;
 import java.util.List;
 
 @Repository
 public interface PaymentRepository extends JpaRepository<Payment, Integer> {
     List<Payment> findAllByUserId(Integer userId);
+    List<Payment> findAllByUserIdAndDateBetween(Integer userId, Date timeStart, Date timeEnd);
 }
